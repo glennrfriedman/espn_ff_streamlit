e05e5e965d5473d796be4370bd9a3d5bd593590c
glennrfriedman <glenn.r.friedman@gmail.com>
2020-10-19 19:35:13 -0400

    fixing setup sh file

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "\
+
+echo “\
 [general]\n\
-email = \"glenn.r.friedman@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+email = \”glenn.r.friedman@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
